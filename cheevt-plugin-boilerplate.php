82da1386c5661213271dea1c26c578f8f6638763
cheevt <cheevt@gmail.com>
2022-04-05 12:53:53 +0200

    plugin init

---
+++ cheevt-plugin-boilerplate.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+Plugin Name: CheeVT Plugin Boilerplate
+Description: Wordpress plugin boilerplate ready for development
+Author: CheeVT
+Version: 1.0.0
+Author URI: https://github.com/CheeVT
+*/
+
+if(! defined('ABSPATH')) exit;
