7c91a020eed3f6e1b316a584f865de752a4827d9
cheevt <cheevt@gmail.com>
2022-05-04 11:36:12 +0200

    shortcode abstract class, add renderView method

--- src/Core/Abstracts/Shortcode.php
+++ src/Core/Abstracts/Shortcode.php
@@ -15,6 +15,17 @@ abstract class Shortcode
         $this->__dir__ = $__dir__;
         add_shortcode($this->shortcode, [$this, 'renderShortcode']);
     }
+
+    protected function renderView($view, $atts = [])
+    {
+        if(is_array($atts)) extract($atts);
+        ob_start();
+        include $this->__dir__ . '/views/shortcodes/' . $view . '.php';
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
     
     abstract public function renderShortcode($atts, $content);
 }
--- src/Shortcodes/ContactFormShortcode.php
+++ src/Shortcodes/ContactFormShortcode.php
@@ -10,13 +10,6 @@ class ContactFormShortcode extends Shortcode
 
     public function renderShortcode($atts, $content)
     {
-        if($atts) extract($atts);
-
-        ob_start();
-        include $this->__dir__ . '/views/shortcodes/contact-form.php';
-        $content = ob_get_contents();
-        ob_end_clean();
-
-        return $content;
+        return $this->renderView('contact-form', $atts);
     }
 }
